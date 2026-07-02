b529ec0c761e541873978c83183fd952a5907fc5
EriikGabriel <erikgabriel.lins@hotmail.com>
2026-07-02 18:45:24 -0300

    commit teste

--- garibot-2/scenes/options/sound_slider/sound_slider.gd
+++ garibot-2/scenes/options/sound_slider/sound_slider.gd
@@ -11,6 +11,3 @@ func _ready() -> void:
 func _on_volume_changed(value : float) -> void:
 		AudioServer.set_bus_volume_db(bus_index, linear_to_db(value/100))
 		pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
